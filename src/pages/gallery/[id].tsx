b621f168a94ba70f57b77b2c536840a7765cfb98
fe-olaf <frontend.olaf@gmail.com>
2021-07-29 21:51:51 +0900

    feature: 갤러리 애니메이션 추가

--- src/pages/gallery/[id].tsx
+++ src/pages/gallery/[id].tsx
@@ -5,6 +5,7 @@ import { Beige } from '$types/theme'
 import styles from './Gallery.module.scss'
 
 import { useWeddingContext } from '$contexts/WeddingContext'
+import Animation from '$shared/Animation'
 
 const cx = classnames.bind(styles)
 
@@ -19,16 +20,18 @@ function GalleryPage() {
 
   return (
     <div>
-      {gallery.map((url, idx) => {
-        return (
-          <img
-            className={cx('image')}
-            key={idx}
-            src={url}
-            alt={`${idx}번 쨰 웨딩 이미지`}
-          />
-        )
-      })}
+      <Animation useAnimation type="fadein">
+        {gallery.map((url, idx) => {
+          return (
+            <img
+              className={cx('image')}
+              key={idx}
+              src={url}
+              alt={`${idx}번 쨰 웨딩 이미지`}
+            />
+          )
+        })}
+      </Animation>
     </div>
   )
 }
